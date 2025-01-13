1afc291a1abcc37e5e6805bee6a2fb152ea76f29
TracerStudySmekdapapa <habibiesanji@gmail.com>
2025-01-13 16:06:48 +0700

    feat: cek dev or prod

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: isProduction
+    ? "https://clerk-wine.vercel.app" // URL untuk lingkungan production
+    : "http://localhost:3000/api", // URL untuk lingkungan development
   withCredentials: true,
 });
