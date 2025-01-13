83e01f0a4ad8a38b836effca1ed6037648c68097
TracerStudySmekdapapa <habibiesanji@gmail.com>
2025-01-13 16:08:49 +0700

    Update axiosInstance.ts

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const axiosInstance = axios.create({
   baseURL: isProduction
-    ? "https://clerk-wine.vercel.app" // URL untuk lingkungan production
+    ? "https://clerk-wine.vercel.app/api" // URL untuk lingkungan production
     : "http://localhost:3000/api", // URL untuk lingkungan development
   withCredentials: true,
 });
