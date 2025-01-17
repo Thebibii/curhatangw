e65974860318f573ce469909cebb95dfe26d84fb
TracerStudySmekdapapa <habibiesanji@gmail.com>
2025-01-18 02:28:19 +0700

    fix: get user clerk id

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -57,7 +57,11 @@ export async function getDbUserId() {
   const { userId: clerkId } = await auth();
   if (!clerkId) return null;
 
-  const user = await getUserByClerkId(clerkId);
+  const user = await prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+  });
 
   if (!user) throw new Error("User not found");
 
